dd27d3354a4952b47f6c2eeb0f49128421c787d7
HuynhLeVu5598 <huynhlevu5598@gmail.com>
2022-09-05 13:50:39 +0700

    'v1'

---
+++ test_copy.py
@@ -0,0 +1,25 @@
+# print('             Class      Images      Labels          P         R      mAP@.5 mAP      ')
+# print('               all         854        1299      0.949     0.918       0.938')
+# print('            di_vat         854         280      0.951     0.864       0.917')
+# print('          bac_truc         854         845          1         1       0.99')
+# print('     tray_bac_truc         854         174      0.924     0.891       0.901')
+# print("It's saved to levu\\runs\\train\\my_result\\my_results2022-07-12_15-20-30-512323")
+
+
+# print('             Class      Images      Labels          P         R      mAP@.5 mAP      ')
+# print('               all         637        1299      0.949     0.918       0.958')
+# print('          nam_cham         637         280      0.951     0.864       0.997')
+# print('            di_vat         637         845          1         1       0.905')
+# print('           nc_tray         637         174      0.924     0.891       0.972')
+# print("It's saved to levu\\runs\\train\\my_result\\my_results2022-07-11_16-24-54-739210")
+
+from tqdm import tqdm
+ 
+ 
+print(' Epoch')
+print('299/299: 100%', end=' ')
+for i in tqdm(range(int(114))):
+    pass
+print('299/299: 100%', end=' ')
+for i in tqdm(range(int(114))):
+    pass
